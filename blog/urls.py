9d44fecf864c00f52a823254a7cc4e8de070add8
Martin Hermelo <hermelomartin@gmail.com>
2022-09-04 02:44:07 -0300

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import Create_note, List_notes, Delete_note
-
+from blog.views import Create_note, List_notes, Delete_note, Detail_note
 urlpatterns = [
-    path("crear-nota/", Create_note.as_view(), name="create_stadium"),
-    path("lista-notas/", List_notes.as_view(), name="list_stadiums"),
-    path("borrar-nota/<int:pk>/", Delete_note.as_view(), name="delete_stadium")
+    path("crear-nota/", Create_note.as_view(), name="create_note"),
+    path("lista-notas/", List_notes.as_view(), name="list_note"),
+    path("borrar-nota/<int:pk>/", Delete_note.as_view(), name="delete_note"),
+    path("detalle-nota/<int:pk>/", Detail_note.as_view(), name="detail_note"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
 from blog.models import Notes
-from django.views.generic import CreateView, ListView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import DeleteView, CreateView
 
 
 class Create_note(CreateView):
@@ -17,3 +17,8 @@ class Delete_note(DeleteView):
     model= Notes
     template_name="notes/delete_note.html"
     success_url= "/notes/lista-notas/"
+
+
+class Detail_note(DetailView):
+    model= Notes
+    template_name="notes/detail_note.html"
